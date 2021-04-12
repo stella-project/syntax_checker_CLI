0fe8a806e329b6751dad6c712248a8f15bc07ad6
Jueri <jueri.keller@smail.th-koeln.de>
2021-04-12 10:09:56 +0200

    add docstrings

--- RunValidator.py
+++ RunValidator.py
@@ -4,10 +4,28 @@ import click
 import random
 
 
-def validator(ranking_str, k=None):
+def validator(run_str, k=None):
+    """Validates a run file string.
+    
+    Args:
+        run_str (str): Ranking string of a run file.
+        k (int): Number random sample lines to validate in-depth.
+
+    Returns:
+        list: List of error messages
+        str: Error message string if the run is empty
+    """
     def _construct_error_string(error_log):
+        """Construct a reasonable in-depth error message from all errors.
+
+        Args:
+            error_log (dict): Dictionary with errors and occurrence counts.
+
+        Returns:
+            list: list of error strings
+        """
         message = []
-        error_count = sum([len(error_log[s]) for s in error_log])
+        error_count = sum([len(error_log[s]) for s in error_log])  # sum all errors for all types
         message.append('There are {} errors in your file:\n'.format(str(error_count)))
         for error_type in error_log:
             if len(error_log[error_type]) > 1:
@@ -21,31 +39,34 @@ def validator(ranking_str, k=None):
     run_tag = {}
     topics = {}
     samples = []
-    if ranking_str:
-        lines = ranking_str.split('\n')[:-1]
+    if run_str:
+        lines = run_str.split('\n')[:-1]
         if k:
             for _ in range(0, k):
-                s = lines[random.randint(0, len(lines) - 1)]
+                s = lines[random.randint(0, len(lines) - 1)]  # Pick k random lines to validate.
                 samples.append(s)
         else:
             samples = lines
 
         with click.progressbar(samples, show_eta=True) as ranking:
             for line in ranking:
+                # Check for and split at delimiter.
                 if '\t' in line:
                     fields = line.split('\t')
                 elif ' ' in line:
                     fields = line.split(' ')
                 else:
-                    error_log.setdefault('wrong delimeter', []).append(
-                        'Error line {} - Could not detect delimeter'.format(str(lines.index(line) + 1)))
+                    error_log.setdefault('wrong delimiter', []).append(
+                        'Error line {} - Could not detect delimiter'.format(str(lines.index(line) + 1)))
                     continue
 
+                # Check if all 6 required fields exist.
                 if len(fields) != 6:
                     error_log.setdefault('missing fields', []).append(
                         'Error line {} - Missing fields'.format(str(lines.index(line) + 1)))
                     continue
 
+                # Check if run tag is valid and consistent.
                 run_tag.setdefault('run_tag', fields[5])
                 if not re.search("^[A-Za-z0-9_.-]{1,24}$", fields[5]):
                     error_log.setdefault('malformed run tag', []).append(
@@ -60,14 +81,16 @@ def validator(ranking_str, k=None):
                         continue
                 # todo: Topic anzahl abgleichen
 
+                # Check for Q0 field.
                 if 'Q0'.casefold() not in fields[1].casefold():
                     error_log.setdefault('Q0', []).append('Error line {} - "Field 2 is {} not "Q0"'.format(
                         str(lines.index(line) + 1), str(fields[1])))
                     continue
 
+                # check if score is a digit.
                 if not fields[3].isdigit():
                     error_log.setdefault('rank', []).append(
-                        'Error line {} - "Column 4 (rank) {} must be an integer"'.format(
+                        'Error line {} - "Column 4 (rank) {} must be a digit"'.format(
                             str(lines.index(line) + 1), str(fields[3])))
                     continue
 
@@ -79,9 +102,9 @@ def validator(ranking_str, k=None):
                 # if(error_log):
                 #     print(error_log)
         if len(error_log) == 0:
-            return False
+            return error_log
     else:
-        return 'Runfile is empty!'
+        return 'Run file is empty!'
     return _construct_error_string(error_log)
 
 
@@ -91,14 +114,20 @@ def cli():
 
 
 @cli.command()
-@click.argument('input', type=click.File('r'), nargs=-1)
+@click.argument('filename', type=click.File('r'), nargs=-1)
 @click.option('--k', default=100, help='limit validation to k randomly chosen lines')
-def validate(input, k):
-    """Check the syntax of a ranking file."""
+def validate(filename, k):
+    """Check the syntax of a run file
+
+    Args:
+        filename: Run file or path to run file.
+        k: Number random sample lines to validate in-depth.
+
+    """
     ranking = str()
 
-    if input:
-        for f in input:
+    if filename:
+        for f in filename:
             while True:
                 chunk = f.read(1024)
                 if not chunk:
@@ -112,13 +141,18 @@ def validate(input, k):
         else:
             click.echo('Validation succeeded!')
     else:
-        click.echo('Which file should be validated?')
+        click.echo('No file for validation specified.')
 
 
 @cli.command()
 @click.argument('filename', type=click.Path(exists=True))
 def split(filename):
-    """Split a run file by topic name into separate files."""
+    """Split a run file by topic name into separate files.
+
+    Args:
+        filename: Run file or path to run file.
+
+    """
     with open(filename, 'r') as infile:
         ranking = infile.read()
 
